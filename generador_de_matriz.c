88fa9126c850483434c29ce36293224d39834da8
Juan Pablo Soto Rojas <23349398+Juan-Pablo-Soto@users.noreply.github.com>
2017-10-30 00:04:07 -0600

    Update generador_de_matriz.c
    No se porque sirve, ni que putas esta pasando. No hay que tocarlo mucho porque se despicha
    =)

--- generador_de_matriz.c
+++ generador_de_matriz.c
@@ -1,4 +1,5 @@
-	#include <stdio.h>
+
+#include <stdio.h>
 
 int main()
 {
@@ -6,54 +7,51 @@ int main()
 FILE *fp;
 	fp = fopen("Mapa850.txt", "r");
 	
-	char control=' ';
 	int ancho;
 	int alto;
 	
 	
-	/*extrae el ancho*/
 	fscanf(fp, "%d", &ancho);
+
 	printf("%d \n",ancho);
 	
-	/*extrae el alto*/
 
 	fscanf(fp, "%d", &alto);
 	printf("%d \n",alto);
 
-	char matriz[alto][ancho];
-	/*extrae el mapa*/
 	int i = 0;
 	int j = 0;
-	int contador = 0;
-	while(!feof(fp)){
-		control = fgetc(fp);
-		
-		
-		printf("iteracion %d \n",contador);
-		contador++;
-		
-		if(control == '\n' || control==' '){
-			printf("aqui hay salto de linea o espacio \n");
+	int c;
+	ancho = ancho * 2;
+	alto = alto * 2;
+	char matriz[alto][ancho];
+	
+	for(i = 0; i < alto; i++) {
+		for(int j = 0; j < ancho; j++){
+			c = fgetc(fp);
+				if(c == '\n' || c ==' '){
 			i++;
 			j = 0;
-			//control = fgetc(fp);
 			}
-		else {
-			printf("aqui inserta en matriz el elemento: %c \n", control);
-			matriz[i][j] = control;
-			j++;
-		}
-		}
-	
-	for(i = 0; i < alto; i++){
-		for(j = 0; j < ancho;j ++){
-			char x = matriz[i][j];
-			printf("%c", x);
+			else{
+				matriz[i][j] = c;			
 			}
-		printf("\n");
-		}
-	
-	
+			}
+			}
+
+			
+			
+			
+
+			
+	for(i = 0; i < alto; i++) {
+		for(j = 0; j < ancho; j++){
+			printf("%c", matriz[i][j]);
+			}
+			printf("\n");
+			
+			}
+
 
 fclose(fp);
 		
@@ -63,4 +61,5 @@ fclose(fp);
         
 return 0;
 }
+
 
